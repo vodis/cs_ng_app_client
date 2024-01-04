1e54afda985fda199f575f1a8cf329a9591dd907
vodis <i40217891@gmail.com>
2024-01-04 23:39:27 +0000

    feat/CSA-5 - Update sidebar
    - update layout
    - update header
    - update sidebar
    - update global utilities
    - add animate-line shared component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: [],
+  styleUrls: ['sidebar.component.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  @Input() isMobileView = false;
+
+  public sidebarLinks = [
+    {
+      name: 'Farming',
+      url: '/farm',
+      isActive: true,
+    },
+    {
+      name: 'Earning',
+      url: '/earn',
+      isActive: false,
+    },
+  ];
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  public trackById(index: number): number {
+    return index;
+  }
+
+  public handleRouteChanging(): void {
+    this.document.body.classList.toggle('_is-locked');
+  }
+}
