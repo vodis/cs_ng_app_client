183320f522af5c285479609524297e27d4396a6b
vodis <i40217891@gmail.com>
2024-02-28 01:22:40 +0000

    feat/CSA-12 - Connect to MFE Wallets
    - update all packages to latest
    - use webpack builds
    - bootstrap project
    - declare mfe-wallets
    - update angular.json
    - add wallets mfe component

---
+++ config/webpack.config.js
@@ -0,0 +1,13 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "mfe-wallets": "http://localhost:3000/remoteEntry.js",
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: false, requiredVersion: 'auto' }),
+  },
+
+});
