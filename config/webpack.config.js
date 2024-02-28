7b6e838f6c1c7e420b6fd416e712e73abd3b7be9
vodis <i40217891@gmail.com>
2024-02-28 01:25:57 +0000

    feat/CSA-12 - Connect to MFE Wallets
    - fix replace path for prod env

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,13 +1,18 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
-
   remotes: {
-    "mfe-wallets": "http://localhost:3000/remoteEntry.js",
+    'mfe-wallets': 'http://localhost:3000/remoteEntry.js',
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: false, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: 'auto',
+    }),
   },
-
 });
